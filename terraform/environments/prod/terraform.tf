50da3b52c7bf4d21b2537fcafdd1980afef0feed
Fabian Vargas <fabian_evm@live.com>
2026-05-01 20:12:08 -0500

    prueba aplicacion de configuraciones tf backend

--- terraform/environments/prod/terraform.tf
+++ terraform/environments/prod/terraform.tf
@@ -8,12 +8,11 @@ terraform {
     }
   }
 
-
   backend "azurerm" {
-    resource_group_name  = "rg-tf-state"
-    storage_account_name = "tfstateprod123"
+    resource_group_name  = "rg-ci-cd-devops-terraform-prueba"
+    storage_account_name = "tfstatecicddevops01"
     container_name       = "tfstate"
-    key                  = "prod.terraform.tfstate"
+    key                  = "prod.tfstate"
   }
 }
 
