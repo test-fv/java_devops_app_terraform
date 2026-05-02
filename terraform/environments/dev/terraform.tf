9a308e2b724d5135e7486bb212fd063ec6085de2
Fabian Vargas <fabian_evm@live.com>
2026-05-01 20:37:39 -0500

    config pipelines ts 2

--- terraform/environments/dev/terraform.tf
+++ terraform/environments/dev/terraform.tf
@@ -9,10 +9,10 @@ terraform {
   }
 
   backend "azurerm" {
-    resource_group_name  = "rg-tf-state"
-    storage_account_name = "tfstateprod123"
+    resource_group_name  = "rg-ci-cd-devops-terraform-prueba"
+    storage_account_name = "tfstatecicddevops01"
     container_name       = "tfstate"
-    key                  = "dev.terraform.tfstate"
+    key                  = "dev.tfstate"
   }
 }
 
