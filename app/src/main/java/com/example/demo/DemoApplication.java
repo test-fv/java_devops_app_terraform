7fdb25d12a95f5e0af09211bad00c9623ddef289
Fabian Vargas <fabian_evm@live.com>
2026-05-03 19:39:41 -0500

    inclusion insstruccion 210 en app

--- app/src/main/java/com/example/demo/DemoApplication.java
+++ app/src/main/java/com/example/demo/DemoApplication.java
@@ -12,8 +12,6 @@ public class DemoApplication {
         SpringApplication.run(DemoApplication.class, args);
     }
 
-
-
     @GetMapping("/health")
     public String health() {
         return "Java App OK en AWS v1 :) :) :) n: :D :O  P: - magical moment !!! ";
